6879f23fcec274e32bcb1feebd54c78775661d83
tu-nv <vantu.bkhn@gmail.com>
2020-05-26 08:04:46 +0000

    refactor to use custom incr_checksum and explicit data pointer with previous built-in cursor, bcc automatically convert hton and ntoh, but some bpf functions (like load_bytes) does not automatically do this, which can cause confusion. Thus we change all to manually convert. prev, we use built-in l3 and l4 checksum update, but it does not works with the manual packet direct access. Thus we use custom incr_checksum to recalculate checksum

--- pptmon/pptmon.c
+++ pptmon/pptmon.c
@@ -16,10 +16,12 @@
    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 */
 
+#define KBUILD_MODNAME "pptmon"
 #include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
 #include <linux/in.h>
-#include <uapi/linux/pkt_cls.h>
-#include <bcc/proto.h>
+#include <linux/pkt_cls.h>
 #include <net/checksum.h>
 
 #define TCP_OPT_LEN_WORD_MAX 15
@@ -27,21 +29,26 @@
 #define PPT_H_SIZE (PPT_H_SIZE_WORD << 2)
 /* use experimental tcp option kind */
 #define PPT_H_KIND 254
-#define PPT_H_EXID 0x0
+/* network byte order */
+#define PPT_H_EXID 0x0000
 #define PPT_H_ONLY (PPT_H_KIND | PPT_H_SIZE << 8 | PPT_H_EXID << 16)
 
-#define ETH_H_SIZE sizeof(struct ethernet_t)
-#define IP_H_SIZE sizeof(struct ip_t)
-#define TCP_H_SIZE sizeof(struct tcp_t)
+#define ETH_H_SIZE sizeof(struct ethhdr)
+#define IP_H_SIZE sizeof(struct iphdr)
+#define TCP_H_SIZE sizeof(struct tcphdr)
 #define IPTCP_H_SIZE (IP_H_SIZE + TCP_H_SIZE)
 
 #define offset_of(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER)
 #define IP_CSUM_OFF (ETH_H_SIZE + \
-                     offset_of(struct ip_t, hchecksum))
+                     offset_of(struct iphdr, check))
 #define TCP_CSUM_OFF (ETH_H_SIZE + IP_H_SIZE + \
-                      offset_of(struct tcp_t, cksum))
+                      offset_of(struct tcphdr, check))
 #define TCP_OFFSET_OFF (ETH_H_SIZE + IP_H_SIZE + 12)
 #define PPT_OFF (ETH_H_SIZE + IP_H_SIZE + TCP_H_SIZE)
+
+#define CURSOR_ADVANCE(_target, _cursor, _len,_data_end) \
+    ({  _target = _cursor; _cursor += _len; \
+        if(_cursor > _data_end) return XDP_DROP; })
 
 struct iptcp_t
 {
@@ -54,105 +61,121 @@ struct ppt_t
     u64 tstamp;
 } __attribute__((packed));
 
+static inline u16 incr_checksum_replace16(u16 old_check, u16 old, u16 new){
+    u32 sum;
+    old_check = ~old_check;
+    old = ~old;
+    sum = (u32)old_check + old + new;
+    sum = (sum & 0xffff) + (sum >> 16);
+    sum += (sum >> 16);
+    return (u16)(~sum & 0xffff);
+}
+
 static inline u16 incr_checksum_add16(u16 old_check, u16 new)
 {
     u32 sum;
-    old_check = ~ntohs(old_check);
+    old_check = ~old_check;
     sum = (u32)old_check + new;
     sum = (sum & 0xffff) + (sum >> 16);
-    return htons(~((u16)(sum >> 16) + (sum & 0xffff)));
+    sum += (sum >> 16);
+    return (u16)(~sum & 0xffff);
 }
+
 
 static inline u16 incr_checksum_add32(u16 old_check, u32 new)
 {
     u32 sum;
-    old_check = ~ntohs(old_check);
+    old_check = ~old_check;
     sum = (u32)old_check + (new >> 16) + (new & 0xffff);
     sum = (sum & 0xffff) + (sum >> 16);
-    return htons(~((u16)(sum >> 16) + (sum & 0xffff)));
+    sum += (sum >> 16);
+    return (u16)(~sum & 0xffff);
 }
+
 
 int mon_ingress(struct __sk_buff *skb)
 {
-    u8 *cursor = 0;
+    void* data_end = (void*)(long)skb->data_end;
+    void* cursor = (void*)(long)skb->data;
 
-    struct ethernet_t *ethhdr = cursor_advance(cursor, ETH_H_SIZE);
-    if (ethhdr->type != ETH_P_IP)
+    struct ethhdr *eth;
+    CURSOR_ADVANCE(eth, cursor, sizeof(*eth), data_end);
+    if (ntohs(eth->h_proto) != ETH_P_IP)
         return TC_ACT_OK;
 
-    struct ip_t *iphdr = cursor_advance(cursor, IP_H_SIZE);
-    if (iphdr->nextp != IPPROTO_TCP)
+    struct iphdr *ip;
+    CURSOR_ADVANCE(ip, cursor, sizeof(*ip), data_end);
+    if (ip->protocol != IPPROTO_TCP)
         return TC_ACT_OK;
 
-    struct tcp_t *tcphdr = cursor_advance(cursor, TCP_H_SIZE);
+    struct tcphdr *tcp;
+    CURSOR_ADVANCE(tcp, cursor, sizeof(*tcp), data_end);
 
     /* check tcp option condition */
-    if (tcphdr->offset > TCP_OPT_LEN_WORD_MAX - PPT_H_SIZE_WORD)
+    if (tcp->doff > TCP_OPT_LEN_WORD_MAX - PPT_H_SIZE_WORD)
         return TC_ACT_OK;
 
-    /*  We want to create space right after TCP header and before TCP option,
-        which bpf_skb_adjust_room does not support yet.
-        Thus, we create space right before IP header (which is supported),
-        then move the IP and TCP (w/o option) header back to correct position
-    */
-    struct iptcp_t iptcphdr = {};
-    bpf_skb_load_bytes(skb, ETH_H_SIZE, &iptcphdr, IPTCP_H_SIZE);
-    bpf_skb_adjust_room(skb, PPT_H_SIZE, BPF_ADJ_ROOM_NET, 0);
-    bpf_skb_store_bytes(skb, ETH_H_SIZE, &iptcphdr, IPTCP_H_SIZE, 0);
-
-    /* add PPT header to the new created space */
+    /* ppt header */
     struct ppt_t ppthdr = {};
     ppthdr.header = PPT_H_ONLY;
     ppthdr.tstamp = skb->tstamp;
-    bpf_skb_store_bytes(skb, ETH_H_SIZE + IPTCP_H_SIZE,
-                        &ppthdr, PPT_H_SIZE, 0);
 
+
+    /*  Because after adjust pkt room, all pointers wil be invalid,
+        we need to update ip and tcp header prior
+     */
     /* update IP header & checksum */
-    u16 old_tlen = iphdr->tlen;
-    u16 new_tlen = iphdr->tlen + (PPT_H_SIZE);
-    bpf_l3_csum_replace(skb,
-                        IP_CSUM_OFF,
-                        htons(old_tlen),
-                        htons(new_tlen),
-                        sizeof(new_tlen));
-    iphdr->tlen = new_tlen;
+    u16 old_ip_tlen = ip->tot_len;
+    u16 new_ip_tlen = htons(ntohs(old_ip_tlen) + (PPT_H_SIZE));
+    ip->tot_len = new_ip_tlen;
+    ip->check = incr_checksum_replace16(ip->check, old_ip_tlen, new_ip_tlen);
 
     /* update TCP header len & checksum */
     /* because offset is only 4 bits, we need to expand to 2 bytes
        for checksum calculation, which include offset, reseved, flags
-    */
+     */
+    u16 csum = tcp->check;
     u16 old_off2flag, new_off2flag;
     bpf_skb_load_bytes(skb,
                        TCP_OFFSET_OFF,
                        &old_off2flag,
                        sizeof(old_off2flag));
-    tcphdr->offset = tcphdr->offset + (PPT_H_SIZE_WORD);
+
+    tcp->doff = tcp->doff + (PPT_H_SIZE_WORD);
+
     bpf_skb_load_bytes(skb,
                        TCP_OFFSET_OFF,
                        &new_off2flag,
                        sizeof(new_off2flag));
-    bpf_l4_csum_replace(skb,
-                        TCP_CSUM_OFF,
-                        old_off2flag,
-                        new_off2flag,
-                        sizeof(new_off2flag));
-    /* tcplen pseudo header */
-    u16 new_tcplen = iphdr->tlen - (iphdr->hlen << 2);
-    u16 old_tcplen = new_tcplen - PPT_H_SIZE;
-    bpf_l4_csum_replace(skb,
-                        TCP_CSUM_OFF,
-                        htons(old_tcplen),
-                        htons(new_tcplen),
-                        sizeof(new_tcplen));
 
-    /* update tcp checksump after adding ppt */
-    u16 csum = tcphdr->cksum;
+    csum = incr_checksum_replace16(csum, old_off2flag, new_off2flag);
+
+    /* tcplen pseudo header */
+    u16 old_tcplen = htons(ntohs(old_ip_tlen) - (ip->ihl << 2));
+    u16 new_tcplen = htons(ntohs(new_ip_tlen) - (ip->ihl << 2));
+    csum = incr_checksum_replace16(csum, old_ip_tlen, new_ip_tlen);
+
+    /* checksump with new ppt */
     csum = incr_checksum_add32(csum, PPT_H_ONLY);
     csum = incr_checksum_add32(csum, ppthdr.tstamp & 0xffffffff);
     csum = incr_checksum_add32(csum, ppthdr.tstamp >> 32);
-    tcphdr->cksum = csum;
 
-EXIT:
+    tcp->check = csum;
+
+    /*  Now we actually create space to add ppt header.
+        We want to create space right after TCP header and before TCP option,
+        which bpf_skb_adjust_room does not support yet.
+        Thus, we create space right before IP header (which is supported),
+        then move the IP and TCP (w/o option) header back to correct position
+    */
+    struct iptcp_t iptcp = {};
+    bpf_skb_load_bytes(skb, ETH_H_SIZE, &iptcp, IPTCP_H_SIZE);
+    bpf_skb_adjust_room(skb, PPT_H_SIZE, BPF_ADJ_ROOM_NET, 0);
+    bpf_skb_store_bytes(skb, ETH_H_SIZE, &iptcp, IPTCP_H_SIZE, 0);
+    /* add PPT header to the new created space */
+    bpf_skb_store_bytes(skb, ETH_H_SIZE + IPTCP_H_SIZE,
+                        &ppthdr, PPT_H_SIZE, 0);
+
     return TC_ACT_OK;
 }
 
