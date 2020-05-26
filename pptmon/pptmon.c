1a02c0b17963be660519c5bb80fbfc5bc3a9016e
tu-nv <vantu.bkhn@gmail.com>
2020-05-26 13:06:33 +0000

    restore packet on egress restore original packet data. not process ppt data yet use __always_inline instead of broken inline refactor checksum functions fix wrong adjust pkt room - it does not move iphdr

--- pptmon/pptmon.c
+++ pptmon/pptmon.c
@@ -36,7 +36,6 @@
 #define ETH_H_SIZE sizeof(struct ethhdr)
 #define IP_H_SIZE sizeof(struct iphdr)
 #define TCP_H_SIZE sizeof(struct tcphdr)
-#define IPTCP_H_SIZE (IP_H_SIZE + TCP_H_SIZE)
 
 #define offset_of(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER)
 #define IP_CSUM_OFF (ETH_H_SIZE + \
@@ -50,48 +49,38 @@
     ({  _target = _cursor; _cursor += _len; \
         if(_cursor > _data_end) return XDP_DROP; })
 
-struct iptcp_t
-{
-    u8 data[IPTCP_H_SIZE];
-} __attribute__((packed));
-
 struct ppt_t
 {
     u32 header;
     u64 tstamp;
 } __attribute__((packed));
 
-static inline u16 incr_checksum_replace16(u16 old_check, u16 old, u16 new){
+static __always_inline u16 incr_csum_replace16(u16 old_csum,
+                                      u16 old_val,
+                                      u16 new_val){
     u32 sum;
-    old_check = ~old_check;
-    old = ~old;
-    sum = (u32)old_check + old + new;
+    old_csum = ~old_csum;
+    old_val = ~old_val;
+    sum = (u32)old_csum + old_val + new_val;
     sum = (sum & 0xffff) + (sum >> 16);
     sum += (sum >> 16);
     return (u16)(~sum & 0xffff);
 }
 
-static inline u16 incr_checksum_add16(u16 old_check, u16 new)
+static __always_inline u16 incr_csum_add32(u16 old_csum, u32 add_val)
 {
     u32 sum;
-    old_check = ~old_check;
-    sum = (u32)old_check + new;
+    old_csum = ~old_csum;
+    sum = (u32)old_csum + (add_val >> 16) + (add_val & 0xffff);
     sum = (sum & 0xffff) + (sum >> 16);
     sum += (sum >> 16);
     return (u16)(~sum & 0xffff);
 }
 
-
-static inline u16 incr_checksum_add32(u16 old_check, u32 new)
+static __always_inline u16 incr_csum_remove32(u16 old_csum, u32 rem_val)
 {
-    u32 sum;
-    old_check = ~old_check;
-    sum = (u32)old_check + (new >> 16) + (new & 0xffff);
-    sum = (sum & 0xffff) + (sum >> 16);
-    sum += (sum >> 16);
-    return (u16)(~sum & 0xffff);
+    return incr_csum_add32(old_csum, ~rem_val);
 }
-
 
 int mon_ingress(struct __sk_buff *skb)
 {
@@ -116,9 +105,9 @@ int mon_ingress(struct __sk_buff *skb)
         return TC_ACT_OK;
 
     /* ppt header */
-    struct ppt_t ppthdr = {};
-    ppthdr.header = PPT_H_ONLY;
-    ppthdr.tstamp = skb->tstamp;
+    struct ppt_t ppt = {};
+    ppt.header = PPT_H_ONLY;
+    ppt.tstamp = skb->tstamp;
 
 
     /*  Because after adjust pkt room, all pointers wil be invalid,
@@ -128,7 +117,7 @@ int mon_ingress(struct __sk_buff *skb)
     u16 old_ip_tlen = ip->tot_len;
     u16 new_ip_tlen = htons(ntohs(old_ip_tlen) + (PPT_H_SIZE));
     ip->tot_len = new_ip_tlen;
-    ip->check = incr_checksum_replace16(ip->check, old_ip_tlen, new_ip_tlen);
+    ip->check = incr_csum_replace16(ip->check, old_ip_tlen, new_ip_tlen);
 
     /* update TCP header len & checksum */
     /* because offset is only 4 bits, we need to expand to 2 bytes
@@ -148,17 +137,17 @@ int mon_ingress(struct __sk_buff *skb)
                        &new_off2flag,
                        sizeof(new_off2flag));
 
-    csum = incr_checksum_replace16(csum, old_off2flag, new_off2flag);
+    csum = incr_csum_replace16(csum, old_off2flag, new_off2flag);
 
     /* tcplen pseudo header */
     u16 old_tcplen = htons(ntohs(old_ip_tlen) - (ip->ihl << 2));
     u16 new_tcplen = htons(ntohs(new_ip_tlen) - (ip->ihl << 2));
-    csum = incr_checksum_replace16(csum, old_ip_tlen, new_ip_tlen);
+    csum = incr_csum_replace16(csum, old_ip_tlen, new_ip_tlen);
 
-    /* checksump with new ppt */
-    csum = incr_checksum_add32(csum, PPT_H_ONLY);
-    csum = incr_checksum_add32(csum, ppthdr.tstamp & 0xffffffff);
-    csum = incr_checksum_add32(csum, ppthdr.tstamp >> 32);
+    /* checksum with new ppt */
+    csum = incr_csum_add32(csum, PPT_H_ONLY);
+    csum = incr_csum_add32(csum, ppt.tstamp & 0xffffffff);
+    csum = incr_csum_add32(csum, ppt.tstamp >> 32);
 
     tcp->check = csum;
 
@@ -168,13 +157,15 @@ int mon_ingress(struct __sk_buff *skb)
         Thus, we create space right before IP header (which is supported),
         then move the IP and TCP (w/o option) header back to correct position
     */
-    struct iptcp_t iptcp = {};
-    bpf_skb_load_bytes(skb, ETH_H_SIZE, &iptcp, IPTCP_H_SIZE);
+    struct tcphdr tcp_buf = {};
+    bpf_skb_load_bytes(skb, ETH_H_SIZE + IP_H_SIZE,
+                       &tcp_buf, TCP_H_SIZE);
     bpf_skb_adjust_room(skb, PPT_H_SIZE, BPF_ADJ_ROOM_NET, 0);
-    bpf_skb_store_bytes(skb, ETH_H_SIZE, &iptcp, IPTCP_H_SIZE, 0);
+    bpf_skb_store_bytes(skb, ETH_H_SIZE + IP_H_SIZE,
+                        &tcp_buf, TCP_H_SIZE, 0);
     /* add PPT header to the new created space */
-    bpf_skb_store_bytes(skb, ETH_H_SIZE + IPTCP_H_SIZE,
-                        &ppthdr, PPT_H_SIZE, 0);
+    bpf_skb_store_bytes(skb, ETH_H_SIZE + IP_H_SIZE + TCP_H_SIZE,
+                        &ppt, PPT_H_SIZE, 0);
 
     return TC_ACT_OK;
 }
@@ -182,6 +173,84 @@ int mon_ingress(struct __sk_buff *skb)
 
 int mon_egress(struct __sk_buff *skb)
 {
-    bpf_trace_printk("[OUT] :\n");
+    void* data_end = (void*)(long)skb->data_end;
+    void* cursor = (void*)(long)skb->data;
+
+    struct ethhdr *eth;
+    CURSOR_ADVANCE(eth, cursor, sizeof(*eth), data_end);
+    if (ntohs(eth->h_proto) != ETH_P_IP)
+        return TC_ACT_OK;
+
+    struct iphdr *ip;
+    CURSOR_ADVANCE(ip, cursor, sizeof(*ip), data_end);
+    if (ip->protocol != IPPROTO_TCP)
+        return TC_ACT_OK;
+
+    struct tcphdr *tcp;
+    CURSOR_ADVANCE(tcp, cursor, sizeof(*tcp), data_end);
+
+    struct ppt_t *ppt;
+    CURSOR_ADVANCE(ppt, cursor, sizeof(*ppt), data_end);
+
+    if (ppt->header != PPT_H_ONLY)
+        return TC_ACT_OK;
+
+
+    /* extract and process ppt data */
+
+    /* restore original packet data */
+    /*  Because after adjust pkt room, all pointers wil be invalid,
+        we need to update ip and tcp header prior
+     */
+    /* update IP header & checksum */
+    u16 old_ip_tlen = ip->tot_len;
+    u16 new_ip_tlen = htons(ntohs(old_ip_tlen) - (PPT_H_SIZE));
+    ip->tot_len = new_ip_tlen;
+    ip->check = incr_csum_replace16(ip->check, old_ip_tlen, new_ip_tlen);
+
+    /* update TCP header len & checksum */
+    /* because offset is only 4 bits, we need to expand to 2 bytes
+       for checksum calculation, which include offset, reseved, flags
+     */
+    u16 csum = tcp->check;
+    u16 old_off2flag, new_off2flag;
+    bpf_skb_load_bytes(skb,
+                       TCP_OFFSET_OFF,
+                       &old_off2flag,
+                       sizeof(old_off2flag));
+
+    tcp->doff = tcp->doff - (PPT_H_SIZE_WORD);
+
+    bpf_skb_load_bytes(skb,
+                       TCP_OFFSET_OFF,
+                       &new_off2flag,
+                       sizeof(new_off2flag));
+
+    csum = incr_csum_replace16(csum, old_off2flag, new_off2flag);
+
+    /* tcplen pseudo header */
+    u16 old_tcplen = htons(ntohs(old_ip_tlen) - (ip->ihl << 2));
+    u16 new_tcplen = htons(ntohs(new_ip_tlen) - (ip->ihl << 2));
+    csum = incr_csum_replace16(csum, old_ip_tlen, new_ip_tlen);
+
+    /* checksum when remove ppt */
+    csum = incr_csum_remove32(csum, PPT_H_ONLY);
+    csum = incr_csum_remove32(csum, ppt->tstamp & 0xffffffff);
+    csum = incr_csum_remove32(csum, ppt->tstamp >> 32);
+
+    tcp->check = csum;
+
+    /*  Now we actually remove ppt header.
+        bpf_skb_adjust_room allows removing space right after eth header.
+        Thus, we move iptcp header right by PPT_H_SIZE, and then remove
+        the space from ETH_H_SIZE to ETH_H_SIZE+PPT_H_SIZE
+    */
+    struct tcphdr tcp_buf = {};
+    bpf_skb_load_bytes(skb, ETH_H_SIZE + IP_H_SIZE,
+                       &tcp_buf, TCP_H_SIZE);
+    bpf_skb_store_bytes(skb, ETH_H_SIZE + IP_H_SIZE + PPT_H_SIZE,
+                        &tcp_buf, TCP_H_SIZE, 0);
+    bpf_skb_adjust_room(skb, -PPT_H_SIZE, BPF_ADJ_ROOM_NET, 0);
+
     return TC_ACT_OK;
 }
